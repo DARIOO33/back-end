d0bd8067bc00db1cacdc77971551481dc9ce30b1
DARIOO33 <darioanwar3@gmail.com>
2024-03-03 13:41:09 +0100

    add number of orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,11 +4,11 @@ const OrderModel = require("../models/orderModel")
 
 
 const createOrder = async (req, res) => {
-    const { fullname, type, detail, income, initialPrice, date } = req.body
+    const { fullname, type, detail, income, initialPrice, date ,norders} = req.body
     const data = await OrderModel.find()
     const count = data.length + 1
     console.log(count);
-    const order = OrderModel.create({ fullname, num: count, type, detail, income, initialPrice, date })
+    const order = OrderModel.create({ fullname, num: count, type, detail, income, initialPrice, date,norders })
     res.status(200).json(order)
 
 }
